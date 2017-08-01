1d5bb9f1c8ab9fa9d6d555713082579f9753036a
Ali Mirlou <alimirloohome2@gmail.com>
2017-08-02 01:04:05 +0430

    changed the app from executing terminal commands to calling pip function

--- pip upgrade all.py
+++ pip upgrade all.py
@@ -1,13 +1,18 @@
 import pip
-from subprocess import run, PIPE
+from io import StringIO
+from contextlib import redirect_stdout
 
 
 def update(package_name):
     if package in ['numpy', 'scipy']:
-        print("Package %s is skipped for compatibility issues\n" % package)
+        print("Package %s is skipped for compatibility issues on windows\n" % package)
         return False
-    print("Updating %s..." % (package_name))
-    print(run("pip install --upgrade %s" % (package_name), shell=True, stdout=PIPE).stdout.decode('utf-8'))
+    print("Updating %s..." % package_name)
+    f = StringIO()
+    with redirect_stdout(f):
+        pip.main(['install', '--upgrade', package_name])
+    output = f.getvalue().split('\n')
+    print(output)
     print("Update Complete")
     return True
 
@@ -17,32 +22,34 @@ def update_all(outdated_packages):
         update(package)
     print("All packages are updated")
 
+if __name__ == "__main__":
+    print("Searching for outdated packages...")
+    f = StringIO()
+    with redirect_stdout(f):
+        pip.main(['list', '--outdated', '--format=columns'])
+    output = f.getvalue().split('\n')
+    header = output[0:2]
+    del output[0:2]
+    del output[-1]
+    index = 1
 
-print("Searching for outdated packages...")
-outdated = []
-output = run("pip list --outdated --format=columns", shell=True, stdout=PIPE).stdout.decode('utf-8').split('\r\n')
-header = output[0:2]
-del output[0:2]
-del output[-1]
-index = 1
-print("Outdated packages: ")
-for package in output:
-    package = package.split()
-    print('%d) %s\t\t\t%s --> %s (%s)' % (index, package[0], package[1], package[2], package[3]))
-    outdated.append(package[0])
-    index += 1
+    print("Outdated packages: ")
+    outdated = []
+    for package in output:
+        package = package.split()
+        print('%d) %s\t\t\t%s --> %s (%s)' % (index, package[0], package[1], package[2], package[3]))
+        outdated.append(package[0])
+        index += 1
 
-while True:
-    choice = input("\nWhich library do you want to update? [all,<number>,exit] ")
-    if choice == 'exit':
-        break
-    if choice == 'all':
-        choice = input("\nAre you sure to update all outdated packages? [y,<else>] ")
-        if choice == 'y':
-            update_all(outdated)
+    while True:
+        choice = input("\nWhich library do you want to update? [all,<number>,exit] ")
+        if choice == 'exit':
             break
-    choices = choice.split(',')
-    for choice in choices:
-        update(outdated[int(choice)-1])
-
-input("Press Enter to exit...")
+        if choice == 'all':
+            choice = input("\nAre you sure to update all outdated packages? [y,<else>] ")
+            if choice == 'y':
+                update_all(outdated)
+                break
+        choices = choice.split(',')
+        for choice in choices:
+            update(outdated[int(choice)-1])
