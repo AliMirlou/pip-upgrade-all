9e101508c6d35812587b0c47dac10343649daa29
Ali Mirlou <alimirloohome2@gmail.com>
2017-08-02 19:30:03 +0430

    tried to follow Unix philosophy because friends asked :)

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # pip-upgrade-all
-Simple and easy-to-use app to update pip packages installed on it.
+Simple and easy-to-use app to update pip packages.
+
+## Requirements
+ - **[python]** (>=3.4)
+ - pip (>=9.0.1)
+
+## Usage
+    $ python pip-upgrade-all.py
+
+## Options
+### `--interactive` `-i`
+Runs the app in a loop till exit or keyboard interrupt. 
+
+### `--ignore_stdout` `-s`
+Silences the output of pip's logging of upgrade progress.
+
+[python]: https://www.python.org/
--- pip upgrade all.py
+++ pip upgrade all.py
@@ -1,55 +1,81 @@
+import platform
+import sys
 import pip
 from io import StringIO
 from contextlib import redirect_stdout
 
-
-def update(package_name):
-    if package in ['numpy', 'scipy']:
-        print("Package %s is skipped for compatibility issues on windows\n" % package)
-        return False
-    print("Updating %s..." % package_name)
-    f = StringIO()
-    with redirect_stdout(f):
-        pip.main(['install', '--upgrade', package_name])
-    output = f.getvalue().split('\n')
-    print(output)
-    print("Update Complete")
-    return True
+outdated = []
+ignore_stdout = False
 
 
-def update_all(outdated_packages):
-    for package in outdated_packages:
-        update(package)
-    print("All packages are updated")
-
-if __name__ == "__main__":
-    print("Searching for outdated packages...")
+def check_outdated_packages():
     f = StringIO()
     with redirect_stdout(f):
         pip.main(['list', '--outdated', '--format=columns'])
-    output = f.getvalue().split('\n')
-    header = output[0:2]
-    del output[0:2]
-    del output[-1]
-    index = 1
+    return f.getvalue().split('\n')[3:-1]
 
-    print("Outdated packages: ")
-    outdated = []
-    for package in output:
+
+def update(package_index):
+    package = outdated[package_index]
+    package_name = package[0]
+    package_new_version = package[1]
+    if platform.system() == 'Windows' and package_name in ['numpy', 'scipy']:
+        print("Error: Package %s is skipped for compatibility issues on windows\n" % package_name)
+        return False
+    print("Updating %s to version %s..." % (package_name, package_new_version))
+    f = StringIO()
+    with redirect_stdout(f):
+        pip.main(['install', '--upgrade', '%s==%s' % (package_name, package_new_version)])
+    if not ignore_stdout:
+        print(f.getvalue().split('\n'))
+        print("Update Complete")
+    return True
+
+
+def update_all():
+    for package_index in range(len(outdated)):
+        update(package_index)
+    print("All packages are updated")
+
+
+def create_outdated_list(_list):
+    index = 1
+    for package in _list:
         package = package.split()
         print('%d) %s\t\t\t%s --> %s (%s)' % (index, package[0], package[1], package[2], package[3]))
-        outdated.append(package[0])
+        outdated.append([package[0], package[2]])
         index += 1
 
-    while True:
-        choice = input("\nWhich library do you want to update? [all,<number>,exit] ")
-        if choice == 'exit':
-            break
-        if choice == 'all':
-            choice = input("\nAre you sure to update all outdated packages? [y,<else>] ")
-            if choice == 'y':
-                update_all(outdated)
-                break
-        choices = choice.split(',')
-        for choice in choices:
-            update(outdated[int(choice)-1])
+
+def choice_menu():
+    choice = input("\nWhich library do you want to update? [all,<number,>,exit] ")
+    if choice == 'exit':
+        return
+    if choice == 'all':
+        choice = input("\nAre you sure to update all outdated packages? [y,<else>] ")
+        if choice == 'y':
+            update_all()
+            return
+    choices = choice.split(',')
+    for choice in choices:
+        try:
+            update(int(choice)-1)
+        except IndexError:
+            print('Error: Choice out of range')
+
+if __name__ == "__main__":
+    print("Searching for outdated packages...")
+    output = check_outdated_packages()
+
+    print("Outdated packages: ")
+    create_outdated_list(output)
+
+    options = sys.argv
+    if len(options) > 1:
+        if '--ignore_stdout' in options or options[1].find('s') != -1:
+            ignore_stdout = True
+        if '--interactive' in options or options[1].find('i') != -1:
+            while True:
+                choice_menu()
+    else:
+        choice_menu()
